eabbcfd140b872b511f1456bdc6a843546d5ead7
Rakib Dhali <rakibdhali343@gmail.com>
2026-01-15 22:26:01 +0600

    some fixes 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Technologies from "./components/technologies";
-import Projects from "./components/projects";
+import Technologies from "./components/Technologies";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import CanvasCursor from "./components/CanvasCursor"; // 👈 import it
-
+import CanvasCursor from "./components/CanvasCursor";
 export default function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
